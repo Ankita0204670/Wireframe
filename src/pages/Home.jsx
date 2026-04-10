5cda9644f62592c238ccf5db8f0d298837d2165b
Ankita0204670 <ankitabhawthankar04@gmail.com>
2026-04-10 20:04:01 +0530

    Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-import fabricImg from "../assets/image1.jpg"; // adjust name if needed
 
 function Home() {
   return (
